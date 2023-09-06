5aa767f1f8930f12d302530de824e8ebc1c85901
BenjaLV <benjalavalla8@gmail.com>
2023-09-06 15:39:07 -0300

    server up

---
+++ dist/controllers/user.controllers.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getUsers = void 0;
+const getUsers = (req, res) => {
+    res.json({
+        msj: "getUsuarios",
+    });
+};
+exports.getUsers = getUsers;
+//# sourceMappingURL=user.controllers.js.map
